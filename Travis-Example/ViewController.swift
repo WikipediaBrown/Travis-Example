a7971589ad59c0a9088d860de3a0f726755148f1
Wikipedia Brown <wikipediabrown@iamgoodbad.com>
2018-01-05 11:00:52 -0800

    Initial Commit

---
+++ Travis-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travis-Example
+//
+//  Created by Wikipedia Brown on 1/5/18.
+//  Copyright © 2018 Wikipedia Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
